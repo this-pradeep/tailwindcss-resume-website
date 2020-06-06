3caf211b84a6802b47d6ae3afc6b826d0482b3c5
the-nayak <60128341+the-nayak@users.noreply.github.com>
2020-06-06 19:29:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'the-secondary' : '#233446',
+        'the-green' : '#3ED27E',
+      },
+      textColor: {
+        'own-blue' : '#96BDD8',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
